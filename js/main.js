e47522b2ae80c1f6fd38eda415b776772fcccb25
Dmitry <98708532+ruinxr9@users.noreply.github.com>
2023-09-08 14:47:21 +0300

    Final version

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let navIconsContainer = document.getElementById('nav-icons');
+let navIcons = document.querySelector('.header-icons');
+navIconsContainer.appendChild(navIcons);
+navIcons.classList.remove('d-none');
